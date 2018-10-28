6c4e4a0e5b124e634413f15b834051a8ad633208
afcjunior <afcjunior95@gmail.com>
2018-10-28 03:23:39 -0300

    implemented this cool little ready-to-use react-redux-loading component

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,30 +1,21 @@
-// export function getInitialData () {
-//   return Promise.all([
-//     _getUsers(),
-//     _getPolls(),
-//   ]).then(([users, polls]) => ({
-//     users: formatUsers(users),
-//     polls: formatPolls(polls),
-//   }))
-// }
-
 import { getInitialData } from '../utils/api'
 import { receivePolls } from './polls'
 import { receiveUsers } from './users'
 import { setAuthedUser } from './authedUser'
-
+import { showLoading, hideLoading} from 'react-redux-loading'
 //mocks one of the users in our mock DB as being authenticated.
 const AUTHED_ID = 'dan_abramov'
 
 //will be a thunk
 export function fetchInitialData(){
   return (dispatch) => {
+    dispatch(showLoading()) //from react-redux-loading
     return getInitialData()
       .then(({ users, polls })=>{
         dispatch(receiveUsers(users))
         dispatch(receivePolls(polls))
         dispatch(setAuthedUser(AUTHED_ID))
-
+        dispatch(hideLoading())  //from react-redux-loading
       })
   }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { fetchInitialData } from '../actions/shared'
 import { connect } from 'react-redux'
 import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
 class App extends Component {
 
   componentDidMount(){
@@ -11,8 +12,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        {
-          this.props.loading === true ? null : <Dashboard />
+        <LoadingBar/>
+        {this.props.loading === true
+          ? null
+          : <Dashboard />
         }
       </div>
     )
